b6acd27cd2ee0b60478edcf7f7e5ca3254cbabc1
sahyro <sahyro@genejunction.com>
2024-08-17 16:11:12 +0800

    First commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
+import { Browser } from '@capacitor/browser';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,26 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  currentCoordinates: { latitude: number; longitude: number } | null = null;
+
   constructor() {}
+
+  async getCurrentPosition() {
+    try {
+      const position = await Geolocation.getCurrentPosition();
+      this.currentCoordinates = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      };
+    } catch (error) {
+      console.error('Error getting location', error);
+    }
+  }
+
+  async openWebPage() {
+    await Browser.open({ url: 'https://ionicframework.com' });
+  }
+  
+ 
 
 }
